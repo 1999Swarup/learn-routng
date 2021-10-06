2d1c1d94f32a25401939fa4dcf71a38f98512379
Swarup Mishra <swarupmishra1999@gmail.com>
2021-10-06 13:20:33 +0530

    Add existing file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learn-routng';
+  title = 'news-site';
+
+  onActivate(event:any) {
+    window.scroll(0,0);
+}
 }
